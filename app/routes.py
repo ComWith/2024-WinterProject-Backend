1c8e8c6529a1f0fbbfceebef0de737c4a8e540b0
yim0327 <yim0327@naver.com>
2025-01-05 23:11:24 +0900

    feat: 모든 악보 조회 api 구현

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from flask import Blueprint
+from app.models import User, MusicSheet
+from flask import jsonify
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return "Hello, Flask!"
+
+@main.route('/users/<int:user_id>/musicsheets', methods=['GET'])
+def get_all_sheets(user_id):
+    user = User.query.get(user_id) # db에서 user_id 찾아오기
+    if not user: # db에 없을 시 None 반환
+        abort(404, description="User not found")
+
+    # 악보 테이블에서 user_id가 일치하는 모든 값을 sheets 리스트에 담기.
+    sheets = MusicSheet.query.filter_by(user_id=user_id).all()
+
+    # 변환된 딕셔너리 리스트를 JSON 형식으로 변환후 반환.
+    return jsonify([MusicSheet.to_dict() for MusicSheet in sheets])
