50909fa807bbcb094bda5d83176cf365024469d4
smiinii <sm020320@naver.com>
2025-01-17 18:55:30 +0900

    chore: cors 로그 추가

--- run.py
+++ run.py
@@ -1,8 +1,25 @@
 from app import create_app
+import logging
+from flask import Flask, request
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # 특정 도메인만 허용
+
+# 로깅 설정
+logging.basicConfig(filename='flask.log', level=logging.INFO)
+
+# CORS 설정
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.before_request
+def log_request_info():
+    app.logger.info(f"Request made to {request.url} with method {request.method}")
+
+@app.after_request
+def after_request(response):
+    if request.method == "OPTIONS":
+        app.logger.info(f"CORS Preflight OPTIONS request received from {request.origin}")
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
