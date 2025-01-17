7d4b5ec2f6d31b289eacb3af0ece83005ee97393
smiinii <sm020320@naver.com>
2025-01-17 20:25:56 +0900

    chore: run.py 수정

--- run.py
+++ run.py
@@ -2,21 +2,18 @@ from app import create_app
 import logging
 from flask import Flask, request
 from flask_cors import CORS
-import os
 
 app = create_app()
 
 # 로깅 설정
 logging.basicConfig(filename='flask.log', level=logging.INFO)
 
-# CORS 설정
-CORS(app, resources={r"/*": {"origins": os.environ.get('FLASK_CORS_ORIGINS', 'http://localhost:3000')}})
-
+# CORS 설정 (모든 출처 허용)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.before_request
 def log_request_info():
     app.logger.info(f"Request made to {request.url} with method {request.method}")
-
 
 @app.after_request
 def after_request(response):
@@ -25,7 +22,7 @@ def after_request(response):
         app.logger.info(f"CORS Preflight OPTIONS request received from {request.origin}")
 
     # CORS 헤더 추가
-    response.headers.add('Access-Control-Allow-Origin', os.environ.get('FLASK_CORS_ORIGINS', 'http://localhost:3000'))
+    response.headers.add('Access-Control-Allow-Origin', '*')  # 모든 출처 허용
     response.headers.add('Access-Control-Allow-Credentials', 'true')  # 쿠키나 자격증명을 허용
 
     return response
